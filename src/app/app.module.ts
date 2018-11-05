a0d68a77701efcb2d2f137813a2496fd70b87fa6
jsteckling56783 <jsteckling@wisc.edu>
2018-11-05 10:26:38 -0600

    Added excel files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeMainComponent } from './home-main/home-main.component';
+import { TitleComponent } from './title/title.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { SidescrollComponent } from './sidescroll/sidescroll.component';
+import { LocationsZoomedOutComponent } from './locations-zoomed-out/locations-zoomed-out.component';
+import { TimeGifComponent } from './time-gif/time-gif.component';
+import { RoutesZoomedOutComponent } from './routes-zoomed-out/routes-zoomed-out.component';
+import { RoutesZoomedInComponent } from './routes-zoomed-in/routes-zoomed-in.component';
+import { NetMovesComponent } from './net-moves/net-moves.component';
+import { DistributionsComponent } from './distributions/distributions.component';
+import { PassholderComponent } from './passholder/passholder.component';
+import { PassholderMonthsComponent } from './passholder-months/passholder-months.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeMainComponent,
+    TitleComponent,
+    SidescrollComponent,
+    LocationsZoomedOutComponent,
+    TimeGifComponent,
+    RoutesZoomedOutComponent,
+    RoutesZoomedInComponent,
+    NetMovesComponent,
+    DistributionsComponent,
+    PassholderComponent,
+    PassholderMonthsComponent
   ],
   imports: [
     BrowserModule,
